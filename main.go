0aca58c57fb851158cf7f5a4a7c65e155ff08a4a
Lyle Douglass <lyle.douglass@gmail.com>
2025-03-22 17:46:33 -0400

    feat: add status message

--- main.go
+++ main.go
@@ -15,6 +15,34 @@ import (
 func onReady(s *discordgo.Session, event *discordgo.Ready) {
 	log.Println("Bot is ready")
 	events.RegisterCommands(s)
+
+	// Set the bot's status to total members with the community role
+	guild, err := s.Guild(viper.GetString("guildID"))
+	if err != nil {
+		log.Println("Error getting guild:", err)
+		return
+	}
+	members, err := s.GuildMembers(guild.ID, "", 1000)
+	if err != nil {
+		log.Println("Error getting guild members:", err)
+		return
+	}
+	roleID := viper.GetString("communityMemberRole")
+	count := 0
+	for _, member := range members {
+		for _, role := range member.Roles {
+			if role == roleID {
+				count++
+				break
+			}
+		}
+	}
+	totalMembers := count
+	status := fmt.Sprintf("Community Memebers: %d", totalMembers)
+	err = s.UpdateCustomStatus(status)
+	if err != nil {
+		log.Println("Error setting status:", err)
+	}
 }
 
 func main() {
@@ -31,11 +59,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("error creating Discord session: %s", err)
 	}
-	// Test Command
+	// Commands and events
 	discord.AddHandler(onReady)
 	discord.AddHandler(commands.InteractionCreate)
 	discord.AddHandler(commands.AddRoleInteractionCreate)
-	discord.AddHandler(events.ButtonInteractionCreate)
+	discord.AddHandler(commands.ListRole)
+	discord.AddHandler(commands.RemoveRole)
+	discord.AddHandler(events.RoleButtonInteractionCreate)
 
 	discord.Open()
 	defer discord.Close()
