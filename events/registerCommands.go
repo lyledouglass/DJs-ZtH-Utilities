0b1c7bfadfb7fddf7dcb73591da09318cfd36847
Lyle Douglass <lyle.douglass@gmail.com>
2025-03-22 17:46:12 -0400

    chore: update command list

--- events/registerCommands.go
+++ events/registerCommands.go
@@ -30,6 +30,36 @@ func RegisterCommands(s *discordgo.Session) {
 				},
 			},
 		},
+		{
+			Name:        "removerole",
+			Description: "Remove a role from a user",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionUser,
+					Name:        "target",
+					Description: "The user to remove the role from",
+					Required:    true,
+				},
+				{
+					Type:        discordgo.ApplicationCommandOptionRole,
+					Name:        "role",
+					Description: "The role to remove from the user",
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "listrole",
+			Description: "List all members with a role",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionRole,
+					Name:        "role",
+					Description: "The role to list members for",
+					Required:    true,
+				},
+			},
+		},
 	}
 	for _, command := range commands {
 		_, err := s.ApplicationCommandCreate(s.State.User.ID, "", command)
