1fc3e702c9cf6d8e5f68f8ff155d81f9d83981e7
Lyle Douglass <lyle.douglass@gmail.com>
2025-03-22 17:45:53 -0400

    feat: updates to button functionality for roles

--- events/buttonInteraction.go
+++ events/buttonInteraction.go
@@ -8,14 +8,14 @@ import (
 	"github.com/spf13/viper"
 )
 
-func ButtonInteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+func RoleButtonInteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	if i.Type == discordgo.InteractionMessageComponent {
 		data := i.MessageComponentData()
-		if strings.HasPrefix(data.CustomID, "approve_role_") {
+		if strings.HasPrefix(data.CustomID, "approve_add_role_") {
 			// Extract targetUserID and roleID from CustomID
 			parts := strings.Split(data.CustomID, "_")
-			targetUserID := parts[2]
-			roleID := parts[3]
+			targetUserID := parts[3]
+			roleID := parts[4]
 
 			// Check if the user has the approver role
 			member, err := s.GuildMember(i.GuildID, i.Member.User.ID)
@@ -54,16 +54,205 @@ func ButtonInteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreat
 				return
 			}
 
-			// Send a message indicating success
-			successMessage := "Role has been successfully added to the user."
+			// Update the embed
+			embed := &discordgo.MessageEmbed{
+				Title:       "Role Request - Approved",
+				Description: "The request to add the `@" + roleID + "` role to " + "<@" + targetUserID + "> has been approved.",
+				Color:       0x00ff00,
+			}
+
+			// Update the message
+			_, err = s.ChannelMessageEditComplex(&discordgo.MessageEdit{
+				ID:      i.Message.ID,
+				Channel: i.ChannelID,
+				Embeds:  &[]*discordgo.MessageEmbed{embed},
+			})
+			if err != nil {
+				log.Println("Error editing message:", err)
+			}
+			// Acknowledge the interaction
 			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Type: discordgo.InteractionResponseUpdateMessage,
 				Data: &discordgo.InteractionResponseData{
-					Content: successMessage,
+					Embeds: []*discordgo.MessageEmbed{embed},
 				},
 			})
 			if err != nil {
 				log.Println("Error sending interaction response:", err)
+			}
+		} else if strings.HasPrefix(data.CustomID, "deny_add_role_") {
+			parts := strings.Split(data.CustomID, "_")
+			targetUserID := parts[2]
+			roleID := parts[3]
+			// Check if the user has the approver role
+			member, err := s.GuildMember(i.GuildID, i.Member.User.ID)
+			if err != nil {
+				log.Println("Error fetching member:", err)
+				return
+			}
+
+			approverRoleId := viper.GetString("roleApproverId")
+			if !contains(member.Roles, approverRoleId) {
+				err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "You do not have permission to deny this request.",
+					},
+				})
+				if err != nil {
+					log.Println("Error sending interaction response:", err)
+				}
+				return
+			}
+
+			// Update the embed
+			embed := &discordgo.MessageEmbed{
+				Title:       "Role Request - Denied",
+				Description: "The request to add <@&" + roleID + "> to <@" + targetUserID + "> has been denied.",
+				Color:       0xff0000,
+			}
+
+			// Update the message
+			_, err = s.ChannelMessageEditComplex(&discordgo.MessageEdit{
+				ID:      i.Message.ID,
+				Channel: i.ChannelID,
+				Embeds:  &[]*discordgo.MessageEmbed{embed},
+			})
+			if err != nil {
+				log.Println("Error editing message:", err)
+			}
+			// Acknowledge the interaction
+			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseUpdateMessage,
+				Data: &discordgo.InteractionResponseData{
+					Embeds: []*discordgo.MessageEmbed{embed},
+				},
+			})
+			if err != nil {
+				log.Println("Error sending interaction response:", err)
+			}
+		}
+		if strings.HasPrefix(data.CustomID, "approve_remove_role_") {
+			parts := strings.Split(data.CustomID, "_")
+			targetUserID := parts[3]
+			roleID := parts[4]
+
+			// Check if the user has the approver role
+			member, err := s.GuildMember(i.GuildID, i.Member.User.ID)
+			if err != nil {
+				log.Println("Error fetching member:", err)
+				return
+			}
+
+			approverRoleId := viper.GetString("roleApproverId")
+			if !contains(member.Roles, approverRoleId) {
+				err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "You do not have permission to approve this request.",
+					},
+				})
+				if err != nil {
+					log.Println("Error sending interaction response:", err)
+				}
+				return
+			}
+
+			// Remove the role from the user
+			err = s.GuildMemberRoleRemove(i.GuildID, targetUserID, roleID)
+			if err != nil {
+				log.Println("Error removing role from user:", err)
+				err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "Failed to remove role from user.",
+					},
+				})
+				if err != nil {
+					log.Println("Error sending interaction response:", err)
+				}
+				return
+			}
+
+			// Update the embed
+			embed := &discordgo.MessageEmbed{
+				Title:       "Role Removal Request - Approved",
+				Description: "The request to remove the `@" + roleID + "` role from " + "<@" + targetUserID + "> has been approved.",
+				Color:       0x00ff00,
+			}
+
+			// Update the message
+			_, err = s.ChannelMessageEditComplex(&discordgo.MessageEdit{
+				ID:      i.Message.ID,
+				Channel: i.ChannelID,
+				Embeds:  &[]*discordgo.MessageEmbed{embed},
+			})
+			if err != nil {
+				log.Println("Error editing message:", err)
+			}
+			// Acknowledge the interaction
+			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseUpdateMessage,
+				Data: &discordgo.InteractionResponseData{
+					Embeds: []*discordgo.MessageEmbed{embed},
+				},
+			})
+			if err != nil {
+				log.Println("Error sending interaction response:", err)
+			}
+		} else if strings.HasPrefix(data.CustomID, "deny_remove_role_") {
+			if strings.HasPrefix(data.CustomID, "deny_remove_role_") {
+				parts := strings.Split(data.CustomID, "_")
+				targetUserID := parts[3]
+				roleID := parts[4]
+
+				// Check if the user has the approver role
+				member, err := s.GuildMember(i.GuildID, i.Member.User.ID)
+				if err != nil {
+					log.Println("Error fetching member:", err)
+					return
+				}
+
+				approverRoleId := viper.GetString("roleApproverId")
+				if !contains(member.Roles, approverRoleId) {
+					err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+						Type: discordgo.InteractionResponseChannelMessageWithSource,
+						Data: &discordgo.InteractionResponseData{
+							Content: "You do not have permission to deny this request.",
+						},
+					})
+					if err != nil {
+						log.Println("Error sending interaction response:", err)
+					}
+					return
+				}
+
+				// Update the embed
+				embed := &discordgo.MessageEmbed{
+					Title:       "Role Removal Request - Denied",
+					Description: "The request to remove <@&" + roleID + "> from <@" + targetUserID + "> has been denied.",
+					Color:       0xff0000,
+				}
+
+				// Update the message
+				_, err = s.ChannelMessageEditComplex(&discordgo.MessageEdit{
+					ID:      i.Message.ID,
+					Channel: i.ChannelID,
+					Embeds:  &[]*discordgo.MessageEmbed{embed},
+				})
+				if err != nil {
+					log.Println("Error editing message:", err)
+				}
+				// Acknowledge the interaction
+				err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseUpdateMessage,
+					Data: &discordgo.InteractionResponseData{
+						Embeds: []*discordgo.MessageEmbed{embed},
+					},
+				})
+				if err != nil {
+					log.Println("Error sending interaction response:", err)
+				}
 			}
 		}
 	}
